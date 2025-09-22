a7c51a38387e11d26e98d59da6348235bf0e2f26
naimeshparikh <nidhivyas999@gmail.com>
2025-09-22 18:56:25 +1200

    old vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'], // 
-  },
-  build: {
-    rollupOptions: {
-      input: './index.html',  // 👈 ensures Netlify knows entry point
-    },
-    outDir: 'dist',            // 
-  server: {
-    port: 3000,
-    open: true
-  },
-  base: '/', // needed so /emi, /sip, etc. resolve correctly
-}
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
